d897f521e4bce156ec522c24e405d4ce69a8dff1
Zeheng Li <imzehengl@gmail.com>
2021-10-11 03:18:39 -0600

    fix db

--- learn_music/settings.py
+++ learn_music/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +87,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(default=os.environ["DATABASE_URL"])
 
 
 # Password validation
