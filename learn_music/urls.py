4ba4b079f3eef10ac3e043af8b76b3e647f9d3a7
Zeheng Li <imzehengl@gmail.com>
2021-10-12 08:34:12 -0600

    clean up

--- learn_music/urls.py
+++ learn_music/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", include(("courses.urls", "courses"), namespace="courses")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
