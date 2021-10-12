87056a349dd8e45d10fa891d47e6c92e0eb9dade
Zeheng Li <imzehengl@gmail.com>
2021-10-12 12:10:10 -0600

    clean up

--- courses/admin.py
+++ courses/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from . import models
 
-# Register your models here.
+
 admin.site.register(models.Course)
 admin.site.register(models.Song)
 admin.site.register(models.Assignment, models.AssignmentAdmin)
 admin.site.register(models.Homework, models.HomeworkAdmin)
+
+admin.site.site_header = "learn-music administration"
