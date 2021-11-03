2004bca92048217bb1624c052ae82ddfd37e8241
Zeheng Li <imzehengl@gmail.com>
2021-11-03 00:01:44 -0600

    use password input

--- courses/forms.py
+++ courses/forms.py
@@ -5,7 +5,7 @@ class LoginForm(forms.Form):
     access_code = forms.CharField(
         label="access_code",
         max_length=50,
-        widget=forms.TextInput(
+        widget=forms.PasswordInput(
             attrs={
                 "class": "w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300",
                 "placeholder": "Your access code",
